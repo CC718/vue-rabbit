6061cfa5e3e9e4ea1861ccd16539b1f328da1911
CC718 <cc86718@163.com>
2023-06-25 20:46:41 +0800

    layout-渲染一级导航列表

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import router from "./router";
 
 import '@/styles/common.scss'
 //测试接口函数
-import { getCategory } from "@/apis//testApi.js";
-getCategory().then((res) => {
-   console.log(res);
-});
+
 const app = createApp(App);
 
 app.use(createPinia());
