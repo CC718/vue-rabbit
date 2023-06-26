826f50e0300e84f8d5247d48dcc1dd5eec46917e
CC718 <cc86718@163.com>
2023-06-26 20:45:27 +0800

    Home组件中的人气推荐和新鲜好物(lazy加载)

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,14 @@ export function getBannerAPI() {
       url: '/home/banner'
    })
 }
+//获取新鲜好物
+export function findNewAPI() {
+   return httpInstance({
+      url: '/home/new'
+   })
+}
+export function getHotAPI() {
+   return httpInstance({
+      url: '/home/hot'
+   })
+}
--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import App from "./App.vue";
 import router from "./router";
 
 import '@/styles/common.scss'
-//测试接口函数
 
+// 引入懒加载指令插件
+import { lazyPlugin } from "@/directives/index.js";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(lazyPlugin)
 
 app.mount("#app");
+
+
+
