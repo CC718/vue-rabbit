3f080c2521d138e767b2d647100f181adf277be6
CC718 <cc86718@163.com>
2023-06-28 17:18:06 +0800

    登录-pinia用户数据持久化(token)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from "./App.vue";
 import router from "./router";
 
@@ -12,7 +12,10 @@ const app = createApp(App);
 //引入全局组件插件
 import { componentPlugin } from "@/components/index.js";
 
-app.use(createPinia());
+//注册持久化插件
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia);
 app.use(router);
 app.use(lazyPlugin)
 app.use(componentPlugin)
