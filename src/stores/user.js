80245bbf42895eba47a87bb1b30518867405f41d
CC718 <cc86718@163.com>
2023-06-28 18:23:47 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,15 @@ export const useUserStore = defineStore('user', () => {
       const res = await loginAPI({ account, password })
       userInfo.value = res.result
    }
-   //return {state ,action}
+   //退出时清除用户信息
+   const clearUserInfo = () => {
+      userInfo.value = {}
+   }
+   //return {state ,action}=>{}
    return {
       userInfo,
-      getUserInfo
+      getUserInfo,
+      clearUserInfo
    }
 },
    {
