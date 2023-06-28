e9964dcd6eeede080ed958cfac87496cd49efd8f
CC718 <cc86718@163.com>
2023-06-29 01:23:09 +0800

    购物车-购物车删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -17,9 +17,21 @@ export const useCartStore = defineStore('cart', () => {
          cartList.value.push(goods)
       }
    }
+
+   // 删除购物车
+   const delCart = (skuId) => {
+      //思路:
+      // 1.找到要删除项的下标值 -splice
+      const idx = cartList.value.findIndex(item => skuId === item.skuId)
+      cartList.value.splice(idx, 1)
+
+      // 2.使用数组的过滤方法 -filter   
+      // cartList.value = cartList.value.filter(item => skuId != item.id)
+   }
    return {
       cartList,
-      addCart
+      addCart,
+      delCart,
    }
 },
    {
