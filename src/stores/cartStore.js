7aa3ade4c7a34a1611e36cae4856e8a523b3f5df
CC718 <cc86718@163.com>
2023-06-29 18:30:56 +0800

    购物车-加入购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,22 +1,35 @@
-
+//封装购物车模块
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
+import { useUserStore } from './user'
+import { insertCartAPI, findNewCartListAPI } from "@/apis/cart";
 export const useCartStore = defineStore('cart', () => {
+   const userStore = useUserStore()
+   const isLogin = computed(() => userStore.userInfo.token)
    //1.定义state - cartList
    const cartList = ref([])
    // 2.定义action --add Cart
-   const addCart = (goods) => {
-      //添加购物车操作
-      //已添加过 - count+1
-      //没有添加过 - 直接push
-      //思路:通过匹配传过来的商品对象中的skuId能不能在cartList中找到,找到了就是添加过
-      const item = cartList.value.find((item) => goods.skuId === item.skuId)
-      if (item) {
-         //找到
-         item.count += goods.count
+   const addCart = async (goods) => {
+      const { skuId, count } = goods
+      if (isLogin.value) {
+         //登录后的加入购物车逻辑
+         await insertCartAPI({ skuId, count })
+         const res = await findNewCartListAPI()
+         cartList.value = res.result
       } else {
-         cartList.value.push(goods)
+         //添加购物车操作
+         //已添加过 - count+1
+         //没有添加过 - 直接push
+         //思路:通过匹配传过来的商品对象中的skuId能不能在cartList中找到,找到了就是添加过
+         const item = cartList.value.find((item) => goods.skuId === item.skuId)
+         if (item) {
+            //找到
+            item.count += goods.count
+         } else {
+            cartList.value.push(goods)
+         }
       }
+
    }
 
    // 删除购物车
@@ -46,7 +59,6 @@ export const useCartStore = defineStore('cart', () => {
    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
    //2.总价 所有项的count*price之和   
    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
-
    // 3.已选择数量
    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a, c) => a + c.count, 0))
    // 4.已选择商品价钱的合计
